70c59544f8750e634116cf014741c87786183a7a
VisPatrick <pksantos1003@gmail.com>
2025-04-17 20:13:32 -0300

    Atividade1

--- aula5/atividade1.py
+++ aula5/atividade1.py
@@ -3,7 +3,7 @@ temperatura = float(input('Informe a temperatura: '))
 
 FAIXA = 15
 
-if temperatura <= FAIXA:
+if temperatura <= FAIXA:  # if ou faz uma coisa ou faz outra
     print('Sistema dentro da faixa de segurança.')
 else:
     print('Necessário manutenção do sistema de refigeração.')
